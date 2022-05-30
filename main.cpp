2d310ca34c3a767385aaa05f1b006a8363a15b67
Jugufugu <utkarsh.asthana55@gmail.com>
2022-05-30 23:43:05 +0530

    sinal hander

--- main.cpp
+++ main.cpp
@@ -1,27 +1,22 @@
-
-
-
-
-
-
 #include<iostream>
 #include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
+#include<stdlib.h>
 
-
+using namespace std;
+void sig_handler(int signum)
+{
+cout<<"Signal Number raised is :"<<signum<<endl;
+exit(0);
+};
 int main()
 {
-
-
-
-
-
-
-
-
-
+alarm(5);
+signal(SIGALRM,sig_handler);
+for(;;)
+sleep(1);
 std::cout<<"Hello World";
-
 return 0;
+}
 
-
-} 
